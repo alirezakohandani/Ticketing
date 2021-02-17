65e55fb831678fbece099006613f13eb21fe0fbc
alirezakohandani <alireza.ko73@gmail.com>
2021-02-17 21:49:31 +0330

    Define the relationship between user models and role

--- Modules/User/Entities/User.php
+++ Modules/User/Entities/User.php
@@ -4,12 +4,21 @@ namespace Modules\User\Entities;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class User extends Model
 {
     use HasFactory;
 
     protected $fillable = ['name', 'email', 'password'];
+
+    /**
+     * The roles that belong to the user
+     */
+    public function roles()
+    {
+        return $this->BelongsToMany(Role::class);
+    }
     
     protected static function newFactory()
     {
