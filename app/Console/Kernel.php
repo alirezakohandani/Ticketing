130422a1298a073435b34e8ac4004d444c75f753
alirezakohandani <alireza.ko73@gmail.com>
2021-02-23 22:23:43 +0330

    Tickets left unanswered by users will be closed.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         \Modules\Ticketing\Console\TicketCommand::class,
+        \Modules\Ticketing\Console\TicketFinishCommand::class,
     ];
 
     /**
