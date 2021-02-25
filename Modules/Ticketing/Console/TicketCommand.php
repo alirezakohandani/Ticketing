419e2740232aed7b241c3d9d1c68ca4902213bee
alirezakohandani <alireza.ko73@gmail.com>
2021-02-25 20:23:32 +0330

    Create tickets and messages for tickets with artisan command

--- Modules/Ticketing/Console/TicketCommand.php
+++ Modules/Ticketing/Console/TicketCommand.php
@@ -15,7 +15,7 @@ class TicketCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'ticket:create {number}';
+    protected $signature = 'ticket:create {numberOfTicket} {numberOfMessage}';
 
     /**
      * The console command description.
@@ -25,29 +25,38 @@ class TicketCommand extends Command
     protected $description = 'create as many tickets as you want';
 
     /**
+     * User variable
+     */
+    private $user;
+
+    /**
      * Create a new command instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(User $user)
     {
+        $this->user = $user;
         parent::__construct();
     }
 
     /**
-     * Execute the console command.
+     * Create tickets and messages for tickets
      *
      * @return mixed
      */
     public function handle()
     {
-        $number = $this->argument('number');
-        for ($i=0; $i <= $number ; $i++) { 
-           $this->createFakeTicket();
+        $numberOfTicket = $this->argument('numberOfTicket');
+        $numberOfMessage = $this->argument('numberOfMessage');
+        for ($i=0; $i < $numberOfTicket ; $i++) { 
+           $ticket = $this->createFakeTicket();
+             for ($j=0; $j < $numberOfMessage ; $j++) { 
+                $this->createFakeMessage($ticket, $j);
+            }
         }
-       
     }
-
+    
     /**
      * Create Fake Ticket
      *
@@ -58,20 +67,49 @@ class TicketCommand extends Command
      */
     private function createFakeTicket()
     {
-        $users = User::orderBy('id')->get('id')->toArray();
-        $id = [];
-        foreach($users as $user)
-        {
-            array_push($id, $user['id']);
-        }
+        $ids = $this->usersIds();
         $type = ['immediate', 'normal', 'nonsignificant'];
         $status = ['pending', 'anwserd', 'finished'];
         return Ticket::create([
-            'user_id' => $id[array_rand($id)],
+            'user_id' => $ids[array_rand($ids)],
             'ref_number' => rand(100000, 999999),
             'type' => $type[array_rand($type)],
             'status' => $status[array_rand($status)],
         ]);
+    }
+
+    /**
+     * Create a fake message for each ticket
+     *
+     * @param Ticket $ticket
+     * @param int $j // Counter
+     * @return Modules\Ticketing\Entities\Ticket
+     */
+    private function createFakeMessage(Ticket $ticket,int $j)
+    {
+        $ids = $this->usersIds();
+        $ticket->messages()->create([
+            'ticket_id' => $ticket->ids,
+            'user_id' => $j === 0 ? $ticket->user_id : $ids[array_rand($ids)],
+            'title' => 'title' . $j,
+            'description' => 'description'. $j,
+        ]);
+    }
+
+    /**
+     * Returns all user ids
+     *
+     * @return array
+     */
+    private function usersIds()
+    {
+        $users_id = $this->user->getAllUsers();
+        $ids = [];
+        foreach($users_id as $id)
+        {
+            array_push($ids, $id->id);
+        }
+        return $ids;
     }
 
     /**
